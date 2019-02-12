24a4275ebe9c970d326c4baff81c9c21988c4175
daniellimws <weesoong.lim@gmail.com>
2019-02-12 12:30:28 +0800

    Fix navbar hrefs

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -8,32 +8,57 @@
     <div class="collapse navbar-collapse w-100 order-1 order-md-0 dual-collapse2" id="navbarColor01">
       <ul class="navbar-nav mr-auto">
       <li class="nav-item" v-bind:class="{ active: tab === 'Challenge' }">
-        <a class="nav-link" href="challenge">Challenge</a>
+        <a class="nav-link" href="/challenge">Challenge</a>
       </li>
       <li class="nav-item" v-bind:class="{ active: tab === 'Leaderboard' }">
-        <a class="nav-link" href="leaderboard">Leaderboard</a>
+        <a class="nav-link" href="/leaderboard">Leaderboard</a>
       </li>
       </ul>
     </div>
 
     <div class="navbar-collapse collapse w-100 order-0 dual-collapse2" id="navbarColor01">
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav ml-auto" v-if="!user">
         <li class="nav-item" v-bind:class="{ active: tab === 'Login' }">
-          <a class="nav-link" href="login">Login</a>
+          <a class="nav-link" href="/login">Login</a>
         </li>
         <li class="nav-item" v-bind:class="{ active: tab === 'Register' }">
-          <a class="nav-link" href="register">Register</a>
+          <a class="nav-link" href="/register">Register</a>
         </li>
       </ul>
+      <span class="ml-auto nav-link text-white" v-if="user">{{ user }}</span>
     </div>
   </nav>
 </template>
 
 <script>
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 export default {
   name: 'Navbar',
+  data() {
+    return {
+      user: ''
+    }
+  },
   props: {
     tab: String
+  },
+  async created() {
+    this.user = getCookie("username");
   }
 }
 </script>
