e366aa6047decd0dda0968519301b2252e41613f
daniellimws <weesoong.lim@gmail.com>
2019-02-12 12:29:24 +0800

    Implement login and register functionality

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,32 +1,36 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/api/user/';
+axios.defaults.withCredentials = true;
+
+const url = '/api/user/';
 
 class UserService {
     // Login 
     static login(username, password) {
-        console.log(username);
-        console.log(password);
         return new Promise(async (resolve, reject) => {
             try {
-                const res = await axios.post(`${url}login`);
-                const data = res.data;
+                const res = await axios.post(`${url}login`, {
+                    username: username,
+                    password: password
+                });
                 resolve(data);
             } catch(err) {
-                reject("Login not implemented");
+                reject("Login failed!");
             }
         })
     }
 
     // Register
-    static register() {
+    static register(username, password) {
         return new Promise(async (resolve, reject) => {
             try {
-                const res = await axios.post(`${url}register`);
-                const data = res.data;
-                resolve(data);
+                const res = await axios.post(`${url}register`, {
+                    username: username,
+                    password: password
+                });
+                resolve(res.data);
             } catch(err) {
-                reject("Register not implemented");
+                reject("Register failed!");
             }
         })
     }
